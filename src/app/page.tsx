f0a83a9c11907ca20cc2a2ef65e03f4efc284beb
Todd Griffin <todd.griffin61@outlook.com>
2023-12-19 12:14:38 +0000

    removed padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { TextArea } from '@radix-ui/themes'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <h1 className='text-xl'>Pixel Tales Story Generator</h1>
       <TextArea placeholder="Enter your story idea" />
       <button className='cursor-pointer' type="button" onClick={() => {console.log('clicked')}}>Get your story!</button>
